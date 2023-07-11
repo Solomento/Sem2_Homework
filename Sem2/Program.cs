545f4fadf129bb3e42e307135f1c3744ad05fd15
Alex <alex.dvoynev2014@yandex.ru>
2023-07-11 16:46:29 +0300

    Задача 2?

--- Sem2/Program.cs
+++ Sem2/Program.cs
@@ -14,7 +14,7 @@ Console.Write($"Новая версия {num} это {num1}");
 */
 
 //Напишите программу, которая выводит третью цифру заданного числа.
-
+/*
 int CutNum(int[] num)
 {
     int count = num.Length;
@@ -29,7 +29,7 @@ int CutNum(int[] num)
     return pos;
 }
 
-Console.Write("Введите колличесво цифр числа: ");
+Console.Write("Введите первые три цифры числа: ");
 int elementsCount = int.Parse(Console.ReadLine());
 int[] num = new int[elementsCount];
 for (int i = 0; i < num.Length; i++)
@@ -39,3 +39,37 @@ for (int i = 0; i < num.Length; i++)
 }
 int num1 = CutNum(num);
 Console.Write("Вывод цифры: " + num1);
+*/
+
+int ThirdDigit(int num)
+{
+    int count = 0; 
+    while(num / 10)
+    {
+        int units = num / 10;
+        int count = units * 10;
+        return units;
+        
+    }
+    
+
+}
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int num1 = ThirdDigit(num);
+Console.Write("Третья цифра: " + num1);
+
+
+//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+/*
+bool DayWeek(int num1)
+{
+    if (num1== 7 || num1== 6) return true;
+    else return false;
+}
+
+Console.Write("Введите день недели по счету: ");
+int num = Convert.ToInt32(Console.ReadLine());
+bool result = DayWeek(num);
+Console.Write(result);
+*/
